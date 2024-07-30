770d9475796010123635db4fcff35e15db46aa09
Tiffany <tgateley22@gmail.com>
2024-07-30 08:15:49 -0500

    if-name-equal-main

--- functions.py
+++ functions.py
@@ -11,3 +11,8 @@ def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
